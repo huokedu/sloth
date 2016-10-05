cb4602aa9ea5fdb038c0f9a736fd8a3b901e7421
Alexander Bronca <alexander.bronca@gmail.com>
2016-10-05 18:03:09 -0400

    Fix channel refresh bug

--- frontend/components/messages/message_feed.jsx
+++ frontend/components/messages/message_feed.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 
-const MessageFeed = ({ channel }) => (
-  <section className="message-feed">
-    <header>
-      <h2>{channel.name}</h2>
-      <small>{channel.members.length} members | {channel.purpose}</small>
-    </header>
-    <p className="message-feed-main">
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-      Messages coming soon :)<br />
-    </p>
-  </section>
-);
+const MessageFeed = ({ channel }) => {
+  if (channel) {
+    return(
+      <section className="message-feed">
+        <header>
+          <h2>{channel.name}</h2>
+          <small>{channel.members.length} members | {channel.purpose}</small>
+        </header>
+        <p className="message-feed-main">
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+          Messages coming soon :)<br />
+        </p>
+      </section>
+    );
+  } else {
+    return <section className="message-feed"></section>;
+  }
+};
 
 export default MessageFeed;
