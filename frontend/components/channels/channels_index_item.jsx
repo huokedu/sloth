f7d706cf68a655eaec101959ed19891c7e92ae8b
Alexander Bronca <alexander.bronca@gmail.com>
2016-10-12 12:17:04 -0400

    Change slothbot icon

--- frontend/components/channels/channels_index_item.jsx
+++ frontend/components/channels/channels_index_item.jsx
@@ -6,10 +6,17 @@ const ChannelsIndexItem = ({ channel, currentChannel }) => {
   const notifications = (channel.notifications) ? "notifications": "";
   const dm = channel.direct ? `direct d${channel.numMembers}` : "indirect";
 
+  let slothbot;
+  if (channel.direct && channel.name === 'slothbot') {
+    slothbot = 'slothbot';
+  } else {
+    slothbot = '';
+  }
+
   return(
     <li className={selection + " " + notifications}>
       <Link
-        className={dm}
+        className={dm + " " + slothbot}
         to={"messages/" + channel.id}>
         {channel.name}
         {(channel.notifications && channel.direct) ? (<span className="notifications-number">{channel.notifications}</span>) : ""}
